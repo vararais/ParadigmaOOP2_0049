774e03e5f6c7c240ec8166c6344465d93ded7406
vararais <varadigmaisnann@gmail.com>
2024-05-28 18:36:18 +0700

    Memanggil Cout dan Menambah Return 0

--- Enkapsulasi/Enkapsulasi.cpp
+++ Enkapsulasi/Enkapsulasi.cpp
@@ -23,6 +23,12 @@ int main()
     lampuRumah.setSaklarNo(3, "Lampu Dapur");
 
 
+    cout << lampuRumah.getSaklarNo(0) << endl;
+    cout << lampuRumah.getSaklarNo(1) << endl;
+    cout << lampuRumah.getSaklarNo(2) << endl;
+    cout << lampuRumah.getSaklarNo(3) << endl;
+
+    return 0;
 }
 
 
