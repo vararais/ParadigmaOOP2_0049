932899b57dba72dab0f47799cd02f65f2579443a
vararais <varadigmaisnann@gmail.com>
2024-05-28 17:47:49 +0700

    Memanggil Seluruh Elemen

--- Polimorpi/Polimorpi.cpp
+++ Polimorpi/Polimorpi.cpp
@@ -22,7 +22,19 @@ public:
 		cout << "Pesan dari Lia" << endl;
 	}
 };
+
 int main()
 {
+	seseorang* obyek;
+	joko a;
+	lia b;
+
+	obyek = &a;
+	obyek->pesan();
+	obyek = &b;
+	obyek->pesan();
+	//a.seseorang::pesan();
+
+	return 0;
 }
 
