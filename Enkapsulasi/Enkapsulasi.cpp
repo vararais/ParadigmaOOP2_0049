b17b646ca436ade9b3cfdac6e7c54453d685f140
vararais <varadigmaisnann@gmail.com>
2024-05-28 18:31:55 +0700

    Memanggil Seluruh Elemen

--- Enkapsulasi/Enkapsulasi.cpp
+++ Enkapsulasi/Enkapsulasi.cpp
@@ -15,7 +15,14 @@ public:
 
 int main()
 {
-    
+    remoteLampu lampuRumah;
+
+    lampuRumah.setSaklarNo(0, "Lampu Teras Rumah");
+    lampuRumah.setSaklarNo(1, "Lampu Ruang Tamu");
+    lampuRumah.setSaklarNo(2, "Lampu Kamar Tidur");
+    lampuRumah.setSaklarNo(3, "Lampu Dapur");
+
+
 }
 
 
