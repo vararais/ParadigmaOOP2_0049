001c44c1b06320f920870def544cd61f83ff27fc
vararais <varadigmaisnann@gmail.com>
2024-05-28 18:41:12 +0700

    Menambah Library dan Class

---
+++ Overriding/Overriding.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
