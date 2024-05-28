b706a1f7daf67901f8faa3c3987813a97ace6d3f
vararais <varadigmaisnann@gmail.com>
2024-05-28 18:49:12 +0700

    Menambahkan Overriding dalam Pewarisan

---
+++ Pewarisan/Pewarisan.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class baseClass {
+public:
+    virtual void perkenalan() {
+        cout << "Hallo saya Function dari base class";
+    }
+};
+
+class derivedClass :public baseClass {
+public:
+    //void perkenalan() {
+    //    cout << "Hallo saya Function dari derived class";
+    //}
+};
+
+int main()
+{
+    derivedClass a;
+    a.perkenalan();
+
+    return 0;
+}
+
+
+
